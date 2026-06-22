5ee72b7d1f89332e2f060c22c1b6a77738fc1bec
mar-031 <marina.mekail@digitalvirgo.com>
2026-06-22 10:11:50 +0200

    Center and resize image

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -56,7 +56,7 @@ function Illustration2() {
     <img
       src={slide2Img}
       alt="Pet health timeline"
-      style={{ width: '100%', maxWidth: 'none', height: 'auto', display: 'block' }}
+      style={{ width: '80%', maxWidth: 'none', height: 'auto', display: 'block', margin: '0 auto' }}
       draggable={false}
     />
   );
