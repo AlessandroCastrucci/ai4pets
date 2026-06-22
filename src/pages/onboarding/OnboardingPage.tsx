e89600d000763d41fe9f94f01e31de03d455393f
mar-031 <marina.mekail@digitalvirgo.com>
2026-06-22 10:54:42 +0200

    Build passes cleanly

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -56,7 +56,7 @@ function Illustration2() {
     <img
       src={slide2Img}
       alt="Pet health timeline"
-      style={{ width: '100%', maxWidth: 'none', height: 'auto', display: 'block' }}
+      style={{ width: '100%', maxWidth: 'none', height: 'auto', display: 'block', marginTop: '80px' }}
       draggable={false}
     />
   );
